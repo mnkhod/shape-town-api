9a8b962df441787a01c67a1abb434967d89dc076
Munkh-Od <mnkhod.dev@gmail.com>
2024-12-16 17:35:15 +0800

    Env Test

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+require('dotenv').config()
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/env-test", (req, res) => res.send(process.env.TEST));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
