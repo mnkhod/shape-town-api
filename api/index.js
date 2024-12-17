4f092acc7f59b65f56dbd64a59a0a1916e715712
Munkh-Od <mnkhod.dev@gmail.com>
2024-12-17 15:17:46 +0800

    Added Cors

--- api/index.js
+++ api/index.js
@@ -25,8 +25,10 @@ const abi = [
 
 const express = require("express");
 const { ethers } = require("ethers")
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 
 app.use(express.static('public'))
 
